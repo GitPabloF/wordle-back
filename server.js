cd047a29f954fab92da64f9cefa7094fc29dbc97
UserWindowsXP <wwwebsurf@gmail.com>
2025-05-11 20:28:25 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,97 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
+
+
+
+const artistes = [
+"The Beatles",
+"bob dylan",
+"Elvis Presley",
+"The Rolling Stones",
+"Jimi Hendrix",
+"Bob Marley",
+"Marvin Gaye",
+"Prince",
+"The Doors",
+"The Who",
+"Nirvana",
+"Madonna",
+"John Lennon",
+"David Bowie",
+"Pink Floyd",
+"Queen",
+"Metallica",
+"The Police",
+"Radiohead",
+"Eminem",
+"Celine Dion",
+"Daft Punk",
+"Ice Cube",
+"Nicki Minaj",
+"Drake",
+"Lil Wayne",
+"Kendrick Lamar",
+"Diplo",
+"Skrillex",
+"Dua Lipa",
+"Stevie Wonder",
+"Aretha Franklin",
+"Michael Jackson",
+"Whitney Houston",
+"Beyonce",
+"Taylor Swift",
+"Ed Sheeran",
+"Billie Eilish",
+"Ariana Grande",
+"Shakira",
+"Justin Bieber",
+"Lady Gaga",
+"Snoop Dogg",
+"Travis Scott",
+"Post Malone",
+"Alicia Keys",
+"The Weeknd",
+"Bruno Mars",
+"Harry Styles",
+"Rosalia",
+"Bad Bunny",
+"BLACKPINK",
+"Rihanna",
+"Adele",
+"Coldplay",
+"Katy Perry",
+"Mariah Carey",
+"Green Day",
+"Linkin Park",
+"Bon Jovi",
+"Phil Collins",
+"Elton John",
+"Usher",
+"Janet Jackson",
+"Tina Turner",
+"George Michael",
+"Pitbull",
+"Daddy Yankee",
+"Avicii",
+"Calvin Harris",
+"Mika",
+"Lenny Kravitz",
+];
+  
+
+
+app.listen(PORT, () => {
+    console.log('server is working')
+})
+
+app.get('/randomArtist', (req, res) =>{
+
+    const random = Math.floor(Math.random() * artistes.length)
+
+    res.status(200).json({artist: artistes[random]})
+
+})
